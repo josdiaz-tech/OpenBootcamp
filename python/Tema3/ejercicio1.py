b0b69f229111c71983e70cc39e97437e9b7f2646
Jose Oliveira <jose281200@hotmail.com>
2022-08-01 11:31:05 -0400

    Ejercicio 1 tema 3

---
+++ python/Tema3/ejercicio1.py
@@ -0,0 +1,31 @@
+
+nombre="Jose"
+apellidos="Oliveira"
+edad=28
+email="jose4921@gmail.com"
+telefono=8390213123
+isCasado=True
+hasHijos=False
+lAmigos=["adrian", "franco", "luis"]
+dicPeliculas={"Pelicula1": "Avengers", "Pelicula2":"Duro de matar", "Pelicula3":"Karate Kid" }
+
+
+print(nombre)
+
+print(apellidos)
+
+print(edad)
+
+print(email)
+
+print(telefono)
+
+print(isCasado)
+
+print(hasHijos)
+
+print(lAmigos)
+
+print(dicPeliculas)
+
+
